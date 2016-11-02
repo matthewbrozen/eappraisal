817ce94ea000517288c85dc596757f2d696010df
matthew brozen <matthewbrozen@gmail.com>
2016-11-02 08:12:57 -0700

    nodemailer changes

--- controllers/reports.js
+++ controllers/reports.js
@@ -33,8 +33,8 @@ function addOne (req, res, next) {
         from: 'TESTING <matthewbrozen@gmail.com>',
         to: 'propertyeappraisal@gmail.com',
         subject: 'You have a client interested in selling their property',
-        text: 'You have a client interested in selling their property... Order: ' + newProperty,
-        html: '<p>ou have a client interested in selling their property with the following details...</p>' + newProperty
+        text: 'You have a client interested in selling their property... Order: ' + newProperty.gross_rent + newProperty.address + newProperty.email,
+        html: '<p>you have a client interested in selling their property with the following details...</p>' + + newProperty.gross_rent + newProperty.address + newProperty.email
       }
 
       transporter.sendMail(mailOptions, function (error, info) {
